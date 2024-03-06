6c542701677287b5807920507e4a5034d74f8ee6
A1 <portnovivan2011@mail.ru>
2024-03-06 21:08:20 +0300

    Added simple test for tessting database migration

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,10 +1,26 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.DirectoryResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Testcontainers
 public abstract class IntegrationTest {
@@ -22,6 +38,22 @@ public abstract class IntegrationTest {
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
         // ...
+        String jdbcUrl = c.getJdbcUrl();
+        String userName = c.getUsername();
+        String password = c.getPassword();
+        Path path =
+            new File(".").toPath().toAbsolutePath().getParent().getParent().resolve("migrations");
+        System.out.println(path);
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            Database database =
+                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new liquibase.Liquibase("master.xml", new DirectoryResourceAccessor(path), database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
