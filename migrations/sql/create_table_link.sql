f60c3647817142d9751f983d5fccf7746ae300e2
A1 <portnovivan2011@mail.ru>
2024-03-07 18:10:08 +0300

    Fixed field name updated_ad -> updated_at

--- migrations/sql/create_table_link.sql
+++ migrations/sql/create_table_link.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS link
 (
     link_id bigint GENERATED ALWAYS AS IDENTITY,
     uri text UNIQUE NOT NULL,
-    updated_ad timestamp WITH TIME ZONE NOT NULL,
+    updated_at timestamp WITH TIME ZONE NOT NULL,
 
     PRIMARY KEY(link_id),
     UNIQUE(uri)
