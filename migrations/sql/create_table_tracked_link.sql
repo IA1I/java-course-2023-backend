054b4a6d28f879bbbf6dc7e80afc059ab8f4083b
A1 <portnovivan2011@mail.ru>
2024-03-10 14:48:03 +0300

    Added new field last_check

--- migrations/sql/create_table_tracked_link.sql
+++ migrations/sql/create_table_tracked_link.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS tracked_link
 (
     chat_id bigint REFERENCES chat ON DELETE CASCADE,
     link_id bigint REFERENCES link,
+    last_check timestamp WITH TIME ZONE NOT NULL,
 
     PRIMARY KEY(chat_id, link_id)
 );
