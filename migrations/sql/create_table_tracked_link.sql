f68fa423cccf00b848222599bc73fd883cba0e50
A1 <portnovivan2011@mail.ru>
2024-03-21 22:25:17 +0300

    Updated migrations to actual

--- migrations/sql/create_table_tracked_link.sql
+++ migrations/sql/create_table_tracked_link.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tracked_link
 (
-    chat_id bigint REFERENCES chat ON DELETE CASCADE,
+    chat_id bigint REFERENCES chat (id) ON DELETE CASCADE,
     link_id bigint REFERENCES link,
 
     PRIMARY KEY(chat_id, link_id)
--- scrapper/src/test/java/edu/java/scrapper/migrations/MigrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/migrations/MigrationTest.java
@@ -3,13 +3,140 @@ package edu.java.scrapper.migrations;
 import edu.java.scrapper.IntegrationTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+@SpringBootTest
 public class MigrationTest extends IntegrationTest {
-    @Test
-    void should2plus2Equals4(){
-        int expected = 4;
-        int actual = 2+2;
+    @Autowired
+    private JdbcClient jdbcClient;
 
-        Assertions.assertThat(actual).isEqualTo(expected);
+    @Test
+    void shouldCheckThatAllTablesAreCreated() {
+        List<Object> expected =
+            List.of("databasechangelog", "databasechangeloglock", "chat", "tracked_link", "link", "question");
+        List<Object> tableNames = jdbcClient.sql(
+                """
+                    SELECT table_name FROM information_schema.tables
+                    WHERE table_schema = 'public'
+                    AND table_catalog = current_database()
+                    """
+            )
+            .query()
+            .singleColumn();
+
+        Assertions.assertThat(tableNames).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void shouldCheckAllCreatedFieldsInTableChat() {
+        Map<String, Object> field1 = new HashMap<>();
+        field1.put("column_name", "id");
+        field1.put("data_type", "bigint");
+        Map<String, Object> field2 = new HashMap<>();
+        field2.put("column_name", "tg_chat_id");
+        field2.put("data_type", "bigint");
+        List<Map<String, Object>> expected = List.of(
+            field1,
+            field2
+        );
+        List<Map<String, Object>> actual = jdbcClient.sql(
+                """
+                    SELECT column_name, data_type FROM information_schema.columns
+                    WHERE table_name ='chat'
+                    """
+            )
+            .query()
+            .listOfRows();
+
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void shouldCheckAllCreatedFieldsInTableLink() {
+        Map<String, Object> field1 = new HashMap<>();
+        field1.put("column_name", "link_id");
+        field1.put("data_type", "bigint");
+        Map<String, Object> field2 = new HashMap<>();
+        field2.put("column_name", "uri");
+        field2.put("data_type", "text");
+        Map<String, Object> field3 = new HashMap<>();
+        field3.put("column_name", "updated_at");
+        field3.put("data_type", "timestamp with time zone");
+        Map<String, Object> field4 = new HashMap<>();
+        field4.put("column_name", "last_check");
+        field4.put("data_type", "timestamp with time zone");
+        List<Map<String, Object>> expected = List.of(
+            field1,
+            field2,
+            field3,
+            field4
+        );
+        List<Map<String, Object>> actual = jdbcClient.sql(
+                """
+                    SELECT column_name, data_type FROM information_schema.columns
+                    WHERE table_name ='link'
+                    """
+            )
+            .query()
+            .listOfRows();
+
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void shouldCheckAllCreatedFieldsInTableQuestion() {
+        Map<String, Object> field1 = new HashMap<>();
+        field1.put("column_name", "link_id");
+        field1.put("data_type", "bigint");
+        Map<String, Object> field2 = new HashMap<>();
+        field2.put("column_name", "comments_count");
+        field2.put("data_type", "integer");
+        Map<String, Object> field3 = new HashMap<>();
+        field3.put("column_name", "answers_count");
+        field3.put("data_type", "integer");
+        List<Map<String, Object>> expected = List.of(
+            field1,
+            field2,
+            field3
+        );
+        List<Map<String, Object>> actual = jdbcClient.sql(
+                """
+                    SELECT column_name, data_type FROM information_schema.columns
+                    WHERE table_name ='question'
+                    """
+            )
+            .query()
+            .listOfRows();
+
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void shouldCheckAllCreatedFieldsInTableTrackedLink() {
+        Map<String, Object> field1 = new HashMap<>();
+        field1.put("column_name", "chat_id");
+        field1.put("data_type", "bigint");
+        Map<String, Object> field2 = new HashMap<>();
+        field2.put("column_name", "link_id");
+        field2.put("data_type", "bigint");
+        List<Map<String, Object>> expected = List.of(
+            field1,
+            field2
+        );
+        List<Map<String, Object>> actual = jdbcClient.sql(
+                """
+                    SELECT column_name, data_type FROM information_schema.columns
+                    WHERE table_name ='tracked_link'
+                    """
+            )
+            .query()
+            .listOfRows();
+
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
     }
 }
